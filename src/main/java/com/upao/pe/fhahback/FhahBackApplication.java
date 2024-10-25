9096b690063a6b919edc9a72e25af3a35b1d4b37
jmlc643 <josemarialuyocampos@gmail.com>
2024-10-24 22:04:49 -0500

    first commit

---
+++ src/main/java/com/upao/pe/fhahback/FhahBackApplication.java
@@ -0,0 +1,13 @@
+package com.upao.pe.fhahback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhahBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhahBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upao/pe/fhahback/FhahBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.pe.fhahback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhahBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
